9047a4566138bbdefb06eff353af80a010afd030
jakep <jakep@10.0.0.152>
2020-06-08 15:55:29 -0800

    remove a commented out line

--- src/main/resources/static/chat-room.js
+++ src/main/resources/static/chat-room.js
@@ -42,7 +42,6 @@ function connect() {
 	var socket = new SockJS('/gs-guide-websocket');
 	stompClient = Stomp.over(socket);
 	stompClient.connect({}, function(frame) {
-//		setConnected(true);
 		console.log('Connected: ' + frame);
 		stompClient.subscribe("/topic/rooms/" + roomId + "/messages", function(greeting) {
 			console.log("RECEIVED: ", greeting);
